14120e55e91b84891e22fb42c5f73503420ca8e5
Qihang <qizeng@ucsd.edu>
2022-12-08 12:24:07 -0800

    bugfix

--- client/migrate.go
+++ client/migrate.go
@@ -7,7 +7,7 @@ import (
 )
 
 func (cli *Client) Migrate(ctx context.Context, containerId string, options types.MigrateOptions) error {
-	resp, err := cli.post(ctx, "/containers/"+containerId+"/checkpoints", nil, options, nil)
+	resp, err := cli.post(ctx, "/containers/"+containerId+"/migrate", nil, options, nil)
 	ensureReaderClosed(resp)
 	return err
 }
